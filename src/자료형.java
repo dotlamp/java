b7740b7cea53da028aa173ba0714866dce57c81e
dotlamp <dotlamp@gmail.com>
2020-08-30 22:51:41 +0900

    자료형

---
+++ src/자료형.java
@@ -0,0 +1,14 @@
+public class 자료형 {
+    public static void main(String[] args){
+        //문자 char
+        //숫자 정수형 byte, short, int, long
+        //숫자 실수형 float, double
+        //논리 boolean
+        //어떤 것 String, Date, Person 등 (참조형)
+        //메모리크기
+        //boolean, byte (1byte)
+        //char, short (2byte)
+        //int, float (3byte)
+        //long, double (4byte)
+    }
+}
